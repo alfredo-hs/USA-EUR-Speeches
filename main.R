0a061b9792b1d46fad3fffc73c8d8b25cd790733
Alfredo H. Sanchez <alhdzsz@gmail.com>
2025-09-25 18:45:21 +0300

    updated figure

--- main.R
+++ main.R
@@ -1,3 +1,7 @@
+# ------------------------------------------------------------------------------
+# Setup Libraries
+# ------------------------------------------------------------------------------
+
 library(tidyverse)
 library(tidytext)
 library(reticulate)
@@ -38,7 +42,7 @@ pipeline <- transformers$pipeline
 zeroshot <- transformers$pipeline(
   task = "zero-shot-classification",
   model = "facebook/bart-large-mnli"
-  )
+)
 
 classify_sentences_adaptive <- function(df, 
                                         classifier, 
@@ -116,7 +120,7 @@ system.time({
 })
 
 europe_df <- classified_df %>% 
-  filter(Europe_score >= .8)
+  filter(Europe.and.European.countries_score >= .8)
 
 system.time({
   europe_df <- classify_sentiment_adaptive(
@@ -127,6 +131,7 @@ system.time({
 })
 
 main <- europe_df %>% 
+  filter(sentiment_label != "NEUTRAL") %>%
   group_by(year, sentiment_label) %>% 
   tally() %>% 
   ungroup() %>% 
@@ -145,28 +150,57 @@ main <- europe_df %>%
 
 main %>% 
   ggplot() +
-    geom_area(
-      aes(x = year, 
-          y = total_s/100),
-      alpha = .2
-    ) +
-    geom_col(
-      aes(x = year, 
-          y = perc, 
-          fill = sentiment_label)
-      , position = position_dodge()
-    ) +
-    scale_y_continuous(
-      name = "Sentences on Europe (%)",
-      sec.axis = sec_axis(~.*100, name = "All Sentences (#)")
-    ) +
-    scale_fill_manual(values = c("darkred", "limegreen")) +
-    labs(
-      x = NULL,
-      fill = "Sentiment:"
-    ) +
-    theme_classic() +
-    theme(legend.position = "bottom")
-  
+  geom_area(
+    aes(x = year, 
+        y = total_s/100),
+    alpha = .2
+  ) +
+  geom_col(
+    aes(x = year, 
+        y = perc, 
+        fill = sentiment_label)
+    , position = position_dodge()
+  ) +
+  scale_y_continuous(
+    name = "Sentences about Europe (%)",
+    sec.axis = sec_axis(~.*100, name = "All Speech Sentences (N)")
+  ) +
+  scale_x_continuous(
+    breaks = seq(min(1950, na.rm = TRUE),
+                 max(2025, na.rm = TRUE),
+                 by = 5)
+  ) +
+  scale_fill_manual(values = c("red", "blue")) +
+  annotate("text", 
+           x = 2010, 
+           y = 5, 
+           label = "Trump's 2025 Speech", 
+           hjust = 0.5, 
+           size = 5, 
+           fontface = "bold") +
+  annotate("segment", 
+           x = 2010, xend = 2025, 
+           y = 4.5, 
+           yend = max(main$perc[main$year == 2025], na.rm = TRUE) + .1, 
+           arrow = arrow(length = unit(0.2, "cm"))) +
+  labs(
+    x = NULL,
+    fill = "Sentiment:",
+    caption = "Visualization by: @alfredo-hs"
+  ) +
+  theme_classic() +
+  theme(legend.position = "top",
+        plot.caption = element_text(
+          hjust = 0,            
+          vjust = 0,            
+          size = 8,
+          face = "italic",
+          colour = "grey40"
+        ),
+        plot.caption.position = "plot")
 
+ggsave("usa_europe.png",
+       width = 20,
+       height = 10,
+       unit = "cm")
 
